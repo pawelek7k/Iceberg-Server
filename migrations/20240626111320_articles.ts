897005f8bd174e9dab1a439c29ed0e6128991470
pawelek <k.pawelek@vp.pl>
2024-07-03 21:56:21 +0200

    Update 20240626111320_articles.ts

--- migrations/20240626111320_articles.ts
+++ migrations/20240626111320_articles.ts
@@ -1,14 +1,24 @@
-// For more information about this file see https://dove.feathersjs.com/guides/cli/knexfile.html
-import type { Knex } from 'knex'
+import type { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('articles', (table) => {
-    table.increments('id')
-
-    table.string('text')
-  })
+    table.increments('id');
+    table.string('title')
+    table.specificType('killers', 'text ARRAY');
+    table.specificType('victims', 'text ARRAY');
+    table.string('date')
+    table.specificType('images', 'text ARRAY');
+    table.string('description')
+    table.string('details')
+    table.string('location')
+    table.string('psychologicalBackground')
+    table.string('policeActions')
+    table.string('lawsuit')
+    table.string('socialReactions')
+    table.string('legacyAndImpact')
+  });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  await knex.schema.dropTable('articles')
+  await knex.schema.dropTable('articles');
 }
